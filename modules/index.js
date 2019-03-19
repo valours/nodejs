bc0a11acad4a26007730cbce18672e50959fd270
Valentin Le FLem <leflem.valentin@gmail.com>
2019-03-19 13:09:06 +0100

    add modules example

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+const mod = require('./mod.js');
+const modValues = require('./mod.values.js');
+
+console.log(mod());
+console.log(modValues);
---
+++ modules/mod.values.js
@@ -0,0 +1,4 @@
+module.exports = {
+  var1: '1',
+  var2: '2'
+}
